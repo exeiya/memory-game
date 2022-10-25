604f765b3d2c2b7a2754f1060ff2a28ba24928cf
Krista Lindholm <krista.lindholm@gmail.com>
2022-10-25 16:38:44 +0300

    Style drawer & add a background picture

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import Container from '@mui/material/Container';
 import Grid from "@mui/material/Grid";
 import Drawer from "@mui/material/Drawer"
+import { Typography } from '@mui/material';
 import WordCard from './WordCard';
 import WinDialog from './WinDialog';
 import SettingsMenu from './SettingsMenu';
 import { useState, useEffect } from 'react';
+import bgpicture from './assets/tic-tac-toe.png';
+import styled from '@emotion/styled';
+
+const TitleBackground = styled.div`
+  position: relative;
+  height: 70px;
+  margin-top: 20px;
+  margin-bottom: 30px;
+  padding-top: 10px;
+
+  &:before {
+    content: "";
+    position: absolute;
+    left: 0;
+    top: -12px;
+    right: 0;
+    bottom: 0;
+    transform: skewY(-9deg);
+    background-color: #4db6ff;
+    box-shadow: 0px 10px #6bc2ff;
+  }
+`;
+
+const TitleText = styled(Typography)`
+  padding-top: 10px;
+  position: relative; 
+  color: white;
+  font-weight: bold; 
+  text-shadow: 1px 1px 4px #001529;
+`
 
 const wordPairs = [
   { id: "1", targetLang: "반려동물", translation: "a pet" },
@@ -92,15 +123,20 @@ function App() {
   }, []).map((v, i) => <Grid key={i} item md={2}>{v}</Grid>)
 
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundImage: `url(${bgpicture})`, height:'100vh' }}>
       <Drawer variant="permanent" anchor="left" sx={{
         width: "250px",
         flexShrink: 0,
         '& .MuiDrawer-paper': {
           width: "250px",
           boxSizing: 'border-box',
+          background: "linear-gradient(150deg, #020024 0%, #5dabf4 50%, #00d4ff 100%)",
+          color: "white",
         },
       }}>
+        <TitleBackground>
+          <TitleText variant="h5" gutterBottom align="center">Memory Game</TitleText>
+        </TitleBackground>
         <SettingsMenu />
       </Drawer>
       <Container style={{ maxHeight: 700, maxWidth: 900}}>
--- src/SettingsMenu.js
+++ src/SettingsMenu.js
@@ -5,7 +5,6 @@ import { Divider } from "@mui/material"
 const SettingsMenu = () => {
   return (
     <Box sx={{ padding: "10px" }}>
-      <Typography variant="h5" gutterBottom sx={{ textAlign: "center" }}>Memory Game</Typography>
       <Divider><Typography variant="button">Settings</Typography></Divider>
     </Box>
   )
